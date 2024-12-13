8ce83aec2d5a05bc98960e4f6afca7c7cf0efd5a
FranGarciaga <frangg_10@hotmail.com>
2024-12-13 12:07:10 +0100

    Config Validations

--- src/main/java/org/example/dtos/AppointmentRequest.java
+++ src/main/java/org/example/dtos/AppointmentRequest.java
@@ -1,12 +1,29 @@
 package org.example.dtos;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 public record AppointmentRequest(
-        @NotNull
-        String date,
-        String time,
+
+      //  @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date must be in the format YYYY-MM-DD-.")
+       // @FutureOrPresent(message = "The appointment date must be today or in the future.")
+      //  @NotBlank(message = "The date cannot be blank")
+       // @NotEmpty(message = "The date cannot be empty.")
+        @NotNull(message = "The date cannot be null.")
+        LocalDate date,
+        //@Pattern(regexp = "^([01]\\d|2[0-3]):([0-5]\\d)$", message = "The time must be in HH:mm format.")
+       // @NotEmpty(message = "The time cannot be empty.")
+        @NotNull(message = "The time cannot be null.")
+        LocalTime time,
+        @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s]+$", message = "The reason must contain only letters.")
+        @NotBlank(message = "The reason cannot be blank")
+        @NotEmpty(message = "The reason cannot be empty.")
+        @NotNull(message = "The reason cannot be null.")
         String reason,
-        int petId) {
+        int petId
+) {
 
 }
--- src/main/java/org/example/dtos/GuardianRequest.java
+++ src/main/java/org/example/dtos/GuardianRequest.java
@@ -1,18 +1,30 @@
 package org.example.dtos;
 
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record GuardianRequest(
+        @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s]+$", message = "The name must contain only letters.")
+        @NotBlank(message = "The name cannot be blank")
+        @NotEmpty(message = "The name cannot be empty.")
         @NotNull(message = "The name cannot be null.")
+        @Size (max = 25, message = "The name cannot be greater than 25 numbers.")
         String name,
+        @NotBlank(message = "The email cannot be blank")
+        @NotEmpty(message = "The email cannot be empty.")
+        @NotNull(message = "The email cannot be null.")
         @Email
         String email,
+        @Pattern(regexp = "\\d+", message = "The phone must contain only numbers.")
+        @NotBlank(message = "The phone cannot be blank")
+        @NotEmpty(message = "The phone cannot be empty.")
         @NotNull
-        @Size(min = 8, message = "The phone cannot be less than 8 numbers.")
+        @Size(min = 8, max = 9, message = "The phone cannot be less than 8 numbers and greater than 9.")
         String phone,
+        @NotBlank(message = "The address cannot be blank")
+        @Size (min = 8, max = 20, message = "The address cannot be less than 8 numbers and greater than 20.")
+        @NotEmpty(message = "The address cannot be empty.")
+        @NotNull
         String address
 ){
 }
