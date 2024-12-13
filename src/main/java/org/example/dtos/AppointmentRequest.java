4e75fb55af505dabd0664b87c654d3112cf06570
Ivan <ivanlopezasdf@gmail.com>
2024-12-13 12:40:20 +0100

    Feat: validations

--- src/main/java/org/example/dtos/AppointmentRequest.java
+++ src/main/java/org/example/dtos/AppointmentRequest.java
@@ -8,14 +8,11 @@ import java.time.LocalTime;
 
 public record AppointmentRequest(
 
-      //  @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date must be in the format YYYY-MM-DD-.")
-       // @FutureOrPresent(message = "The appointment date must be today or in the future.")
-      //  @NotBlank(message = "The date cannot be blank")
-       // @NotEmpty(message = "The date cannot be empty.")
+
+        @FutureOrPresent(message = "The appointment date must be today or in the future.")
         @NotNull(message = "The date cannot be null.")
         LocalDate date,
-        //@Pattern(regexp = "^([01]\\d|2[0-3]):([0-5]\\d)$", message = "The time must be in HH:mm format.")
-       // @NotEmpty(message = "The time cannot be empty.")
+        @FutureOrPresent(message = "The appointment time must be in the future")
         @NotNull(message = "The time cannot be null.")
         LocalTime time,
         @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s]+$", message = "The reason must contain only letters.")
